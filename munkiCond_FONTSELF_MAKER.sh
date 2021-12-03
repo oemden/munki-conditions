d42ce2e9ea13c17b9fa4ef0439a51683c4cc03fa
Olivier Emsellem <oemden@users.noreply.github.com>
2021-12-03 01:00:28 +0100

    Update munkiCond_FONTSELF_MAKER.sh
    Adapting from CADTools plugin

--- munkiCond_FONTSELF_MAKER.sh
+++ munkiCond_FONTSELF_MAKER.sh
@@ -2,13 +2,13 @@
 #
 # oem at oemden dot com - Munki Conditions
 #
-# Writes Names conditions : CADTools presence for reinstall
+# Writes Names conditions : FONTSELFMAKER presence for reinstall
 # we need to check the presence of the Apps for plugins to get installed or not.
 ## - - - - - - - - - - - - - - - - - - - - - - -
 #
 version="1.0" #
 ################################################
-## Conditions in manifest: 
+## Conditions in manifest:
 ## INSTALLFONTSELF == 'TRUE'
 ################################################
 DEFAULTS=/usr/bin/defaults
@@ -37,21 +37,21 @@ for CC_VERSION in "${CC_VERSIONS[@]}"
      elif [[ "${CC_VERSION}" -ge 2020 ]] ; then
       ILLUSTRATOR_CC_DIRNAME="Adobe Illustrator"
     fi
-   ILLUSTRATOR_CC_CADTOOLS_PLUGIN_PATH="/Applications/${ILLUSTRATOR_CC_DIRNAME} ${CC_VERSION}/Plug-ins.localized/CADtools.aip"
+   ILLUSTRATOR_CC_FONTSELFMAKER_PLUGIN_PATH="/Applications/${ILLUSTRATOR_CC_DIRNAME} ${CC_VERSION}/Plug-ins.localized/AIHostAdapter.aip"
    ILLUSTRATOR_CC_APP="/Applications/${ILLUSTRATOR_CC_DIRNAME} ${CC_VERSION}/Adobe Illustrator.app"
 
-  echo "---- CADTools plugin Check for ${ILLUSTRATOR_CC_APP} Starting ----"
+  echo "---- FONTSELFMAKER plugin Check for ${ILLUSTRATOR_CC_APP} Starting ----"
 
 if [[ -e "${ILLUSTRATOR_CC_APP}" ]] ; then #01 (illustrator app)
 
  echo " Illustrator CC ${CC_VERSION} is installed" ; echo
- 
- extraecho "ILLUSTRATOR_CC_CADTOOLS_PLUGIN_PATH: ${ILLUSTRATOR_CC_CADTOOLS_PLUGIN_PATH}"
+
+ extraecho "ILLUSTRATOR_CC_FONTSELFMAKER_PLUGIN_PATH: ${ILLUSTRATOR_CC_FONTSELFMAKER_PLUGIN_PATH}"
  extraecho "ILLUSTRATOR_CC_APP: ${ILLUSTRATOR_CC_APP}"
- 
-  if [[ ! -d "${ILLUSTRATOR_CC_CADTOOLS_PLUGIN_PATH}" ]] ; then #(cadtools not installed)
+
+  if [[ ! -d "${ILLUSTRATOR_CC_FONTSELFMAKER_PLUGIN_PATH}" ]] ; then #(FONTSELFMAKER not installed)
    echo
-   echo " CADTOOLS ${CADTOOL_VERSION} plugin is not installed for Illustrator CC ${CC_VERSION}"
+   echo " FONTSELFMAKER ${CADTOOL_VERSION} plugin is not installed for Illustrator CC ${CC_VERSION}"
    echo "Needs to be installed" ; echo
    INSTALLFONTSELF="TRUE"
    extraecho "INSTALLFONTSELF: ${INSTALLFONTSELF}"
@@ -62,12 +62,12 @@ fi
 
 done
 
-if [[ "${INSTALLFONTSELF}" == "TRUE" ]] ; then #02 (cadtools installed)
-   echo " CADTOOLS plugin is missing for some version of Illustrator"
+if [[ "${INSTALLFONTSELF}" == "TRUE" ]] ; then #02 (FONTSELFMAKER installed)
+   echo " FONTSELFMAKER plugin is missing for some version of Illustrator"
    "${DEFAULTS}" write "${COND_DOMAIN}" "INSTALLFONTSELF" TRUE
 fi
 
-# CRITICAL! Since 'defaults' outputs a binary plist, we need to ensure that munki can read it by 
+# CRITICAL! Since 'defaults' outputs a binary plist, we need to ensure that munki can read it by
 # converting it to xml
 plutil -convert xml1 "${COND_DOMAIN}".plist
 
